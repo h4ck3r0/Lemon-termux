0e6190b945591f8fe10ef6aa4aa811e072eedbaf
H4Ck3R <46929618+h4ck3r0@users.noreply.github.com>
2022-06-16 19:13:54 +0530

    Update lemon.sh

--- lemon.sh
+++ lemon.sh
@@ -19,14 +19,14 @@ echo '
                            github.com/h4ck3r0
                            
 ' | lolcat                           
-                                              
+}                                              
  echo ""
  echo -e "\e[1;31m  [\e[32m√\e[31m] \e[1;91m by \e[1;36mRaj Aryan \e[93m/ \e[100;92m Youtube.com/c/H4Ck3R0\e[0m"
  Sleep 10.0
-clear 
-echo -e "\e[34m[\e[92m✓\e[34m]\033[92m INSTALLING REQUIREED PACKAGES"
-apt install nodejs nano yarn -y
-Sleep 5.0
+ clear 
+ echo -e "\e[34m[\e[92m✓\e[34m]\033[92m INSTALLING REQUIREED PACKAGES"
+ apt install nodejs nano yarn -y
+ Sleep 5.0
 
 
 banner ( ) { 
